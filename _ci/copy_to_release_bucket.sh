837a8eba630d65f6026146a94a306e95ee370f5a
Bogdan.Manole <bogdan.manole@ness.com>
2021-04-29 11:14:22 +0300

    ACS-1506 initial setup

---
+++ _ci/copy_to_release_bucket.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "========================== Starting Copy to Release Bucket Script ==========================="
+set -ev
+
+export AWS_ACCESS_KEY_ID="${RELEASE_AWS_ACCESS_KEY}"
+export AWS_SECRET_ACCESS_KEY="${RELEASE_AWS_SECRET_KEY}"
+
+# Identify latest annotated tag (latest version)
+export VERSION="$(git describe --tags `git rev-list --tags --max-count=1`)"
+
+SOURCE="s3://alfresco-artefacts-staging/alfresco-file-transfer-receiver/release/${VERSION}"
+DESTINATION="s3://eu.dl.alfresco.com/release/alfresco-file-transfer-receiver/${VERSION}"
+
+echo "Source:      ${SOURCE}"
+echo "Destination: ${DESTINATION}"
+
+aws s3 cp --acl private "${SOURCE}" "${DESTINATION}" --recursive --include "*.amp"
+
+echo "========================== Finishing Copy to Release Bucket Script =========================="
+
---
+++ _ci/init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Init Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+mkdir -p ${HOME}/.m2 && cp -rf .travis.settings.xml ${HOME}/.m2/settings.xml
+echo "${QUAY_PASSWORD}" | docker login -u="${QUAY_USERNAME}" --password-stdin quay.io
+echo "${DOCKERHUB_PASSWORD}" | docker login -u="${DOCKERHUB_USERNAME}" --password-stdin
+find "${HOME}/.m2/repository/" -type d -name "*-SNAPSHOT*" | xargs -r -l rm -rf
+
+# Enable experimental docker features (e.g. squash options)
+echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
+sudo service docker restart
+
+popd
+set +vex
+echo "=========================== Finishing Init Script =========================="
