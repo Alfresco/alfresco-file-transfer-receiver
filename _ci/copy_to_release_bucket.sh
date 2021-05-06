22205a655e86cf680c14ff716aa39fc4fcd05255
Bogdan.Manole <bogdan.manole@ness.com>
2021-05-06 12:25:15 +0300

    test release [release] [publish]

--- _ci/copy_to_release_bucket.sh
+++ _ci/copy_to_release_bucket.sh
@@ -6,7 +6,7 @@ export AWS_ACCESS_KEY_ID="${RELEASE_AWS_ACCESS_KEY}"
 export AWS_SECRET_ACCESS_KEY="${RELEASE_AWS_SECRET_KEY}"
 
 # Identify latest annotated tag (latest version)
-export VERSION="$(git describe --tags `git rev-list --tags --max-count=1`)"
+export VERSION="$(git describe --tags `git rev-list --tags --max-count=1` | cut -b 21-100)"
 
 SOURCE="s3://alfresco-artefacts-staging/alfresco-file-transfer-receiver/release/${VERSION}"
 DESTINATION="s3://eu.dl.alfresco.com/release/alfresco-file-transfer-receiver/${VERSION}"
@@ -14,7 +14,7 @@ DESTINATION="s3://eu.dl.alfresco.com/release/alfresco-file-transfer-receiver/${V
 echo "Source:      ${SOURCE}"
 echo "Destination: ${DESTINATION}"
 
-aws s3 cp --acl private "${SOURCE}" "${DESTINATION}" --recursive --include "*.amp"
+aws s3 cp --acl private "${SOURCE}" "${DESTINATION}" --recursive --include "*.zip"
 
 echo "========================== Finishing Copy to Release Bucket Script =========================="
 
