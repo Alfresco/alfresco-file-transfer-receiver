4ed844c5fc4f8ca818d106c05446b6ce8bcd73aa
Bogdan.Manole <bogdan.manole@ness.com>
2021-05-07 12:19:53 +0300

    reverted pom versions and travis file changes after test releases

--- _ci/copy_to_release_bucket.sh
+++ _ci/copy_to_release_bucket.sh
@@ -9,7 +9,7 @@ export AWS_SECRET_ACCESS_KEY="${RELEASE_AWS_SECRET_KEY}"
 export VERSION="$(git describe --tags `git rev-list --tags --max-count=1` | cut -b 21-100)"
 
 SOURCE="s3://alfresco-artefacts-staging/alfresco-file-transfer-receiver/releases/${VERSION}"
-DESTINATION="s3://eu.dl.alfresco.com/releases/alfresco-file-transfer-receiver/${VERSION}"
+DESTINATION="s3://eu.dl.alfresco.com/release/alfresco-file-transfer-receiver/${VERSION}"
 
 echo "Source:      ${SOURCE}"
 echo "Destination: ${DESTINATION}"
