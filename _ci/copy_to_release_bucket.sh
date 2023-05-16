8cfa8ac7b2674e6e9837758a1735bfe984cd98a4
Domenico Sibilio <domenicosibilio@gmail.com>
2023-05-16 16:07:42 +0200

    Fix S3 Release/Downloads Publish [publish] (#19)

--- _ci/copy_to_release_bucket.sh
+++ _ci/copy_to_release_bucket.sh
@@ -5,7 +5,7 @@ set -ev
 # Identify latest annotated tag (latest version)
 export VERSION="$(git describe --tags `git rev-list --tags --max-count=1` | cut -b 21-100)"
 
-SOURCE="s3://alfresco-artefacts-staging/alfresco-file-transfer-receiver/releases/${VERSION}"
+SOURCE="s3://alfresco-artefacts-staging/alfresco-file-transfer-receiver/release/${VERSION}"
 DESTINATION="s3://eu.dl.alfresco.com/release/alfresco-file-transfer-receiver/${VERSION}"
 
 echo "Source:      ${SOURCE}"
